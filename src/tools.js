4eef7711f1932a76f108f4ea8f933a1ae7f3d035
rheotommy <tommyrheo@gmail.com>
2024-05-13 21:10:58 +0900

    add: added WebBrowser and test func

--- src/tools.js
+++ src/tools.js
@@ -1,3 +1,6 @@
 import { DuckDuckGoSearch } from "@langchain/community/tools/duckduckgo_search";
+import { WebBrowser } from "langchain/tools/webbrowser";
+import { gemini, gemini_embedding } from "./models.js";
 export const searchTool = new DuckDuckGoSearch();
+export const webBrowser = new WebBrowser({ model: gemini, embeddings: gemini_embedding });
 //# sourceMappingURL=tools.js.map
--- src/tools.ts
+++ src/tools.ts
@@ -1,3 +1,7 @@
 import {DuckDuckGoSearch} from "@langchain/community/tools/duckduckgo_search";
+import {WebBrowser} from "langchain/tools/webbrowser";
+import {gemini, gemini_embedding} from "./models.js";
 
 export const searchTool = new DuckDuckGoSearch();
+
+export const webBrowser = new WebBrowser({model: gemini, embeddings: gemini_embedding})
