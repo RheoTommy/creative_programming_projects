95c8ecad79ef53a5c8780f17fb6190553d71c34c
rheotommy <tommyrheo@gmail.com>
2024-07-10 09:09:36 +0900

    chore: .jsをGitから除外

--- src/tools.js
+++
@@ -1,6 +0,0 @@
-import { DuckDuckGoSearch } from "@langchain/community/tools/duckduckgo_search";
-import { WebBrowser } from "langchain/tools/webbrowser";
-import { geminiPro, geminiEmbedding } from "./models.js";
-export const searchTool = new DuckDuckGoSearch();
-export const webBrowser = new WebBrowser({ model: geminiPro, embeddings: geminiEmbedding });
-//# sourceMappingURL=tools.js.map
