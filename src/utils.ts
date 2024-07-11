f3adcab6ab5eb77005a15f08634b174285a002eb
rheotommy <tommyrheo@gmail.com>
2024-07-11 15:22:00 +0900

    feat: 出力を schema に変換する関数を作成

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
 import {IterableReadableStream} from "@langchain/core/utils/stream";
+import {TypeOf, ZodObject, ZodRawShape} from "zod";
+import {geminiFlash} from "./models.js";
 
 export const streamToConsole = async <T>(stream: IterableReadableStream<T>): Promise<void> => {
     for await (const chunk of stream) {
@@ -13,4 +15,14 @@ export const streamToArray = async <T>(stream: IterableReadableStream<T>): Promi
         res.push(chunk);
     }
     return res;
+}
+
+export const rewriteWithSchema = async <T extends ZodRawShape>(
+    schema: ZodObject<T>,
+    rawOutput: string,
+): Promise<TypeOf<ZodObject<T>>> => {
+    const model = geminiFlash.withStructuredOutput(schema);
+    const prompt = `Rewrite the following text with the schema:\n\n${rawOutput}\n\n`;
+    const res = await model.invoke(prompt);
+    return schema.parse(res);
 }
