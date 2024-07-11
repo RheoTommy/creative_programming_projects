4a5cc3222a8d16c71e8a9f1b8d6bcebc69bc0aad
rheotommy <tommyrheo@gmail.com>
2024-07-11 15:39:50 +0900

    wip

--- tests/tasks.test.ts
+++ tests/tasks.test.ts
@@ -1,5 +1,27 @@
-import {describe, it} from "vitest";
-import tasks from "../tasks/noToolTasks.json"
+import {describe, expect, it} from "vitest";
+import {z} from "zod";
+import {gpt4o} from "../src/models.js";
+import {StringOutputParser} from "@langchain/core/output_parsers";
+import {rewriteWithSchema} from "../src/utils.js";
 
 describe("No Tool Tasks", () => {
+    const schema = z.object({
+        answer: z.string(),
+    });
+    const tasks = [
+        {
+            question: "日本の首都である都道府県名を答えよ",
+            expected: "東京都",
+        },
+        {
+            question: "RustでStackに保存される型に期待されるトレイト名を答えよ",
+            expected: "Copy",
+        }
+    ];
+
+    tasks.forEach(async ({question, expected}) => it(`question: ${question}`, async () => {
+        const raw = await gpt4o.pipe(new StringOutputParser()).invoke(question);
+        const res = await rewriteWithSchema(schema, raw);
+        expect(res.answer).toBe(expected);
+    }));
 })
